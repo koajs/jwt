acc55b9c37b31d56288465af2f0e3094ec9aaaf2
Scott Donnelly <scott@donnel.ly>
2017-01-21 09:14:25 +0000

    arrow functions and const/let where appropriate

--- test/test.js
+++ test/test.js
@@ -7,10 +7,10 @@ const assert  = require('assert');
 const jwt     = require('jsonwebtoken');
 const koajwt  = require('../lib');
 
-describe('failure tests', function () {
+describe('failure tests', () => {
 
-  it('should throw 401 if no authorization header', function(done) {
-    var app = new Koa();
+  it('should throw 401 if no authorization header', done => {
+    const app = new Koa();
 
     app.use(koajwt({ secret: 'shhhh' }));
     request(app.listen())
@@ -19,8 +19,8 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should return 401 if authorization header is malformed', function(done) {
-    var app = new Koa();
+  it('should return 401 if authorization header is malformed', done => {
+    const app = new Koa();
 
     app.use(koajwt({ secret: 'shhhh' }));
     request(app.listen())
@@ -31,12 +31,13 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should allow provided getToken function to throw', function(done) {
-    var app = new Koa();
+  it('should allow provided getToken function to throw', done => {
+    const app = new Koa();
 
-    app.use(koajwt({ secret: 'shhhh', getToken: function(ctx) {
-      ctx.throw(401, 'Bad Authorization\n');
-    } }));
+    app.use(koajwt({
+      secret: 'shhhh',
+      getToken: ctx => ctx.throw(401, 'Bad Authorization\n')
+    }));
     request(app.listen())
       .get('/')
       .expect(401)
@@ -44,13 +45,13 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should throw if getToken function returns invalid jwt', function(done) {
-    var app = new Koa();
+  it('should throw if getToken function returns invalid jwt', done => {
+    const app = new Koa();
 
-    app.use(koajwt({ secret: 'shhhhhh', getToken: function() {
-      var secret = 'bad';
-      return jwt.sign({foo: 'bar'}, secret);
-    } }));
+    app.use(koajwt({
+      secret: 'shhhhhh',
+      getToken: () => jwt.sign({foo: 'bar'}, 'bad')
+    }));
     request(app.listen())
       .get('/')
       .expect(401)
@@ -58,8 +59,8 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should throw if authorization header is not well-formatted jwt', function(done) {
-    var app = new Koa();
+  it('should throw if authorization header is not well-formatted jwt', done => {
+    const app = new Koa();
 
     app.use(koajwt({ secret: 'shhhh' }));
     request(app.listen())
@@ -70,11 +71,11 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should throw if authorization header is not valid jwt', function(done) {
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+  it('should throw if authorization header is not valid jwt', done => {
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
     app.use(koajwt({ secret: 'different-shhhh', debug: true }));
     request(app.listen())
@@ -83,19 +84,16 @@ describe('failure tests', function () {
       .expect(401)
       .expect('Invalid token - invalid signature\n')
       .end(done);
-      //   assert.equal(err.message, 'invalid signature');
   });
 
-  it('should throw if opts.cookies is set and the specified cookie is not well-formatted jwt', function(done) {
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+  it('should throw if opts.cookies is set and the specified cookie is not well-formatted jwt', done => {
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
     app.use(koajwt({ secret: secret, cookie: 'jwt' }));
-    app.use(function (ctx, next) {
-      ctx.body = ctx.state.user;
-    });
+    app.use(ctx => { ctx.body = ctx.state.user; });
 
     request(app.listen())
       .get('/')
@@ -106,13 +104,17 @@ describe('failure tests', function () {
 
   });
 
-  it('should throw if audience is not expected', function(done) {
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar', aud: 'expected-audience'}, secret);
+  it('should throw if audience is not expected', done => {
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar', aud: 'expected-audience'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
+    app.use(koajwt({
+      secret: 'shhhhhh',
+      audience: 'not-expected-audience',
+      debug: true
+    }));
 
-    app.use(koajwt({ secret: 'shhhhhh', audience: 'not-expected-audience', debug: true }));
     request(app.listen())
       .get('/')
       .set('Authorization', 'Bearer ' + token)
@@ -121,13 +123,13 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should throw if token is expired', function(done) {
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar', exp: 1382412921 }, secret);
+  it('should throw if token is expired', done => {
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar', exp: 1382412921 }, secret);
 
-    var app = new Koa();
-
+    const app = new Koa();
     app.use(koajwt({ secret: 'shhhhhh', debug: true }));
+
     request(app.listen())
       .get('/')
       .set('Authorization', 'Bearer ' + token)
@@ -136,13 +138,13 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should throw if token issuer is wrong', function(done) {
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar', iss: 'http://foo' }, secret);
+  it('should throw if token issuer is wrong', done => {
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar', iss: 'http://foo' }, secret);
 
-    var app = new Koa();
-
+    const app = new Koa();
     app.use(koajwt({ secret: 'shhhhhh', issuer: 'http://wrong', debug: true }));
+
     request(app.listen())
       .get('/')
       .set('Authorization', 'Bearer ' + token)
@@ -151,13 +153,13 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should throw if secret neither provided by options or middleware', function (done) {
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar', iss: 'http://foo' }, secret);
+  it('should throw if secret neither provided by options or middleware', done => {
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar', iss: 'http://foo' }, secret);
 
-    var app = new Koa();
-
+    const app = new Koa();
     app.use(koajwt({debug: true}));
+
     request(app.listen())
       .get('/')
       .set('Authorization', 'Bearer ' + token)
@@ -166,13 +168,13 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should throw if secret both provided by options (right secret) and middleware (wrong secret)', function (done) {
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar', iss: 'http://foo' }, secret);
+  it('should throw if secret both provided by options (right secret) and middleware (wrong secret)', done => {
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar', iss: 'http://foo' }, secret);
 
-    var app = new Koa();
-
+    const app = new Koa();
     app.use(koajwt({secret: 'wrong secret', debug: true}));
+
     request(app.listen())
       .get('/')
       .set('Authorization', 'Bearer ' + token)
@@ -181,13 +183,13 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should throw 401 if isRevoked throw error', function(done) {
+  it('should throw 401 if isRevoked throw error', done => {
 
     const isRevoked = (ctx, token, user) => Promise.reject(new Error('Revoked token'));
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
     app.use(koajwt({ secret: secret, isRevoked, debug: true }));
 
@@ -199,13 +201,13 @@ describe('failure tests', function () {
       .end(done);
   });
 
-  it('should throw 401 if revoked token', function(done) {
+  it('should throw 401 if revoked token', done => {
 
     const isRevoked = (ctx, token, user) => Promise.resolve(true);
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
     app.use(koajwt({ secret: secret, isRevoked, debug: true }));
 
@@ -219,12 +221,12 @@ describe('failure tests', function () {
 
 });
 
-describe('passthrough tests', function () {
-  it('should continue if `passthrough` is true', function(done) {
-    var app = new Koa();
+describe('passthrough tests', () => {
+  it('should continue if `passthrough` is true', done => {
+    const app = new Koa();
 
     app.use(koajwt({ secret: 'shhhhhh', passthrough: true, debug: true }));
-    app.use(function (ctx) {
+    app.use(ctx => {
       ctx.body = ctx.state.user;
     });
 
@@ -237,20 +239,18 @@ describe('passthrough tests', function () {
 });
 
 
-describe('success tests', function () {
+describe('success tests', () => {
 
-  it('should work if authorization header is valid jwt', function(done) {
-    var validUserResponse = function(res) {
-      if (!(res.body.foo === 'bar')) return "Wrong user";
-    }
+  it('should work if authorization header is valid jwt', done => {
+    const validUserResponse = res => res.body.foo !== 'bar' && "Wrong user";
 
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
     app.use(koajwt({ secret: secret }));
-    app.use(function (ctx) {
+    app.use(ctx => {
       ctx.body = ctx.state.user;
     });
 
@@ -263,19 +263,15 @@ describe('success tests', function () {
 
   });
 
-  it('should work if the provided getToken function returns a valid jwt', function(done) {
-    var validUserResponse = function(res) {
-      if (!(res.body.foo === 'bar')) return "Wrong user";
-    }
+  it('should work if the provided getToken function returns a valid jwt', done => {
+    const validUserResponse = res => res.body.foo !== 'bar' && "Wrong user";
 
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
-    app.use(koajwt({ secret: secret, getToken: function(ctx) {
-      return ctx.request.query.token;
-    }}));
-    app.use(function(ctx) {
+    const app = new Koa();
+    app.use(koajwt({ secret: secret, getToken: ctx => ctx.request.query.token }));
+    app.use(ctx => {
       ctx.body = ctx.state.user;
     });
 
@@ -286,19 +282,17 @@ describe('success tests', function () {
       .end(done);
   });
 
-  it('should use the first resolved token', function(done) {
-    var validUserResponse = function(res) {
-      if (!(res.body.foo === 'bar')) return "Wrong user";
-    }
+  it('should use the first resolved token', done => {
+    const validUserResponse = res => res.body.foo !== 'bar' && "Wrong user";
 
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var invalidToken = jwt.sign({foo: 'bar'}, 'badSecret');
+    const invalidToken = jwt.sign({foo: 'bar'}, 'badSecret');
 
-    var app = new Koa();
+    const app = new Koa();
     app.use(koajwt({ secret: secret, cookie: 'jwt'}));
-    app.use(function (ctx) {
+    app.use(ctx => {
       ctx.body = ctx.state.user;
     });
 
@@ -311,18 +305,16 @@ describe('success tests', function () {
       .end(done);
   });
 
-  it('should work if opts.cookies is set and the specified cookie contains valid jwt', function(done) {
-    var validUserResponse = function(res) {
-      if (!(res.body.foo === 'bar')) return "Wrong user";
-    }
+  it('should work if opts.cookies is set and the specified cookie contains valid jwt', done => {
+    const validUserResponse = res => res.body.foo !== 'bar' && "Wrong user";
 
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
     app.use(koajwt({ secret: secret, cookie: 'jwt' }));
-    app.use(function(ctx) {
+    app.use(ctx => {
       ctx.body = ctx.state.user;
     });
 
@@ -335,18 +327,16 @@ describe('success tests', function () {
 
   });
 
-  it('should use provided key for decoded data', function(done) {
-    var validUserResponse = function(res) {
-      if (!(res.body.foo === 'bar')) return "Key param not used properly";
-    }
+  it('should use provided key for decoded data', done => {
+    const validUserResponse = res => res.body.foo === 'bar' && "Key param not used properly";
 
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
     app.use(koajwt({ secret: secret, key: 'jwtdata' }));
-    app.use(function (ctx) {
+    app.use(ctx => {
       ctx.body = ctx.state.jwtdata;
     });
 
@@ -359,22 +349,20 @@ describe('success tests', function () {
 
   });
 
-  it('should work if secret is provided by middleware', function (done) {
-    var validUserResponse = function(res) {
-      if (!(res.body.foo === 'bar')) return "Wrong user";
-    };
+  it('should work if secret is provided by middleware', done => {
+    const validUserResponse = res => res.body.foo !== 'bar' && "Wrong user";
 
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
-    app.use(function (ctx, next) {
+    app.use((ctx, next) => {
         ctx.state.secret = secret;
         return next();
     });
     app.use(koajwt());
-    app.use(function(ctx) {
+    app.use(ctx => {
       ctx.body = ctx.state.user;
     });
 
@@ -387,22 +375,20 @@ describe('success tests', function () {
   });
 
 
-  it('should use middleware secret if both middleware and options provided', function (done) {
-    var validUserResponse = function(res) {
-      if (!(res.body.foo === 'bar')) return "Wrong user";
-    };
+  it('should use middleware secret if both middleware and options provided', done => {
+    const validUserResponse = res => res.body.foo !== 'bar' && "Wrong user";
 
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
-    app.use(function(ctx, next) {
+    app.use((ctx, next) => {
       ctx.state.secret = secret;
       return next();
     });
     app.use(koajwt({secret: 'wrong secret'}));
-    app.use(function (ctx) {
+    app.use(ctx => {
       ctx.body = ctx.state.user;
     });
 
@@ -415,20 +401,16 @@ describe('success tests', function () {
   });
 });
 
-describe('unless tests', function () {
+describe('unless tests', () => {
 
-  it('should pass if the route is excluded', function(done) {
-    var validUserResponse = function(res) {
-      if (!(res.body.success === true)) return "koa-jwt is getting fired.";
-    };
+  it('should pass if the route is excluded', done => {
+    const validUserResponse = res => res.body.success === true && "koa-jwt is getting fired.";
 
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
-
-    var app = new Koa();
+    const secret = 'shhhhhh';
+    const app = new Koa();
 
     app.use(koajwt({ secret: secret }).unless({ path: ['/public']}));
-    app.use(function(ctx) {
+    app.use(ctx => {
       ctx.body = { success: true };
     });
 
@@ -440,14 +422,14 @@ describe('unless tests', function () {
       .end(done);
   });
 
-  it('should fail if the route is not excluded', function(done) {
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+  it('should fail if the route is not excluded', done => {
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
     app.use(koajwt({ secret: secret }).unless({ path: ['/public']}));
-    app.use(function(ctx) {
+    app.use(ctx => {
       ctx.body = { success: true };
     });
 
@@ -459,18 +441,16 @@ describe('unless tests', function () {
       .end(done);
   });
 
-  it('should pass if the route is not excluded and the token is present', function(done) {
-    var validUserResponse = function(res) {
-      if (!(res.body.foo === 'bar')) return "Key param not used properly";
-    };
+  it('should pass if the route is not excluded and the token is present', done => {
+    const validUserResponse = res => res.body.foo !== 'bar' && "Key param not used properly";
 
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
     app.use(koajwt({ secret: secret, key: 'jwtdata' }).unless({ path: ['/public']}));
-    app.use(function(ctx) {
+    app.use(ctx => {
       ctx.body = ctx.state.jwtdata;
     });
 
@@ -483,20 +463,18 @@ describe('unless tests', function () {
 
   });
 
-  it('should work if authorization header is valid jwt and is not revoked', function(done) {
-    var validUserResponse = function(res) {
-      if (!(res.body.foo === 'bar')) return "Wrong user";
-    }
+  it('should work if authorization header is valid jwt and is not revoked', done => {
+    const validUserResponse = res => res.body.foo !== 'bar' && "Wrong user";
 
-    var isRevoked = (token, ctx, user) => Promise.resolve(false);
+    const isRevoked = (token, ctx, user) => Promise.resolve(false);
 
-    var secret = 'shhhhhh';
-    var token = jwt.sign({foo: 'bar'}, secret);
+    const secret = 'shhhhhh';
+    const token = jwt.sign({foo: 'bar'}, secret);
 
-    var app = new Koa();
+    const app = new Koa();
 
     app.use(koajwt({ secret: secret, isRevoked }));
-    app.use(function (ctx) {
+    app.use(ctx => {
       ctx.body = ctx.state.user;
     });
 
@@ -508,5 +486,5 @@ describe('unless tests', function () {
       .end(done);
 
   });
-  
+
 });
